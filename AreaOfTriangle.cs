77ddc1782ca7310b28bfde33ce9f1775559550c6
Tariro Grace Shumba <tarirograceshumba2@gmail.com>
2025-01-31 15:20:37 -0600

    Initial Commit
    Initial Commit

---
+++ AreaOfTriangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Triangle_Farmer
+{
+    public class AreaOfTriangle
+    {
+        // class variables 
+        private double trianglebase = 0.0;
+        private double triangleheight = 0.0;
+
+        // getter and setters 
+        public double Trianglebase
+        {
+            get { return this.trianglebase; }
+            set { this.trianglebase = value; }
+        }
+        public double Triangleheight
+        {
+            get { return this.triangleheight; }
+            set { this.triangleheight = value; }
+        }
+
+        public double CalcArea()
+        {
+            double area = (this.trianglebase * this.triangleheight) / 2.0;
+
+            return area;
+        }
+        // constructors 
+
+        public AreaOfTriangle(double aTriangleheight, double aTrianglebase)
+        {
+            this.Trianglebase = aTrianglebase;
+            this.Triangleheight = aTriangleheight;
+        }
+        public override string ToString()
+        {
+            string msg = " ";
+            msg += "Triangle Area : " + CalcArea();
+
+            return msg;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace The_Triangle_Farmer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<AreaOfTriangle> aListOfTriangles = new List<AreaOfTriangle>();
+
+            AreaOfTriangle aTriangles1 = new AreaOfTriangle(2,8);
+            AreaOfTriangle aTriangles2 = new AreaOfTriangle(10, 8);
+
+            aListOfTriangles.Add(aTriangles1);
+            aListOfTriangles.Add(aTriangles2);
+
+            foreach (AreaOfTriangle aTriangle in aListOfTriangles)
+            {
+                Console.WriteLine(aTriangle.ToString());
+            }
+
+
+        }
+    }
+}
