1ca870c4984e9e83dd70bbeeeacfefb9b52754ba
Tariro Grace Shumba <tarirograceshumba2@gmail.com>
2025-02-02 14:28:18 -0600

    Creating the Four Sisters and the duckbear

---
+++ The Four Sisters and the Duckbear/EggCalculation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Four_Sisters_and_the_Duckbear
+{
+    public class EggCalculation
+    {
+        //class variables
+        private int numberofEggs = 0;
+
+        //getters and setters
+
+        public int NumberofEggs
+        {
+            get { return this.numberofEggs; }
+            set
+            {
+                if(value >= 0)
+                {
+                    this.numberofEggs = value;
+                }
+                else
+                {
+                    this.numberofEggs = 0;
+                }
+            }
+        }
+        // methods
+
+        public int CalcEggsForSisters()
+        {
+            int eggsforSisters = this.NumberofEggs / 2;
+            return eggsforSisters;
+            
+        }
+        public int Calceggsforduckbear()
+        {
+            int eggsforDuckbear = this.NumberofEggs % 2;
+            return eggsforDuckbear;
+        }
+
+        // contstructors
+        public EggCalculation(int aEgg)
+        {
+            this.NumberofEggs = aEgg;
+        }
+
+        public override string ToString()
+        {
+            string msg = " ";
+
+            msg += "Eggs for each sister : " + CalcEggsForSisters();
+            msg += "Eggs for duckbear : " + Calceggsforduckbear();
+            return msg;
+        }
+    }
+}
---
+++ The Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace The_Four_Sisters_and_the_Duckbear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many eggs where collected today? :"); // prompting the user
+
+            //putting the users input into a variable and converted the input to integer from string
+            int numberofEggs = Convert.ToInt32(Console.ReadLine()); // putting the users input into a variable
+         
+
+            // creating an instance of egg calculation
+            EggCalculation eggCalculation1 = new EggCalculation(numberofEggs);
+
+            //printong the results using tostring
+            Console.WriteLine(eggCalculation1.ToString());
+
+
+
+        }
+    }
+}
