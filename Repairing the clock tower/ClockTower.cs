52224bda7ae93b3dbd6fd23189d70bef6a5072ef
Tariro Grace Shumba <tarirograceshumba2@gmail.com>
2025-02-02 14:27:08 -0600

    Creating Repairing the clock tower

---
+++ Repairing the clock tower/ClockTower.cs
@@ -0,0 +1,51 @@
+﻿// written by Tariro Grace Shumba
+//2/1/2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repairing_the_clock_tower
+{
+    public class ClockTower
+    {
+        // class variable
+        private int ticktock = 0;
+
+        //getters and setters 
+        public int Ticktock
+        {
+            get { return this.ticktock; }
+            set { this.ticktock = value; }
+        }
+        //methods
+        public string CalctickTock()
+        {
+            if (this.Ticktock % 2 == 0)
+            {
+
+                return $"Tick";
+            }
+            else
+            {
+                 return $"Tock";
+            }
+        }
+        // constructor
+        public ClockTower(int aTicktock)
+        {
+            this.Ticktock = aTicktock;
+        }
+        // overriding tostring
+
+        public override string ToString()
+        {
+            string msg = " ";
+            msg += $" {CalctickTock()} ";
+            return msg;
+        }
+
+    }
+
+}
---
+++ Repairing the clock tower/Program.cs
@@ -0,0 +1,24 @@
+﻿// written by Tariro Grace Shumba
+//2/1/2025
+namespace Repairing_the_clock_tower
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //promptimg the user to enter the ticktock
+            Console.Write("Enter the input: ");
+
+            // reading the input into the variable
+            int ticktock = Convert.ToInt32(Console.ReadLine());
+
+            // creating an instance of the class 
+            ClockTower aClock = new ClockTower(ticktock);
+
+            //printing the output
+            Console.WriteLine(aClock.ToString());
+
+
+        }
+    }
+}
