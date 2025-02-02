75908c147b6724ba7bfc0934c37edd2a85c59303
Tariro Grace Shumba <tarirograceshumba2@gmail.com>
2025-02-02 14:28:00 -0600

    Creating the Dominion of Kings

---
+++ The Dominion Of Kings/DominionOfKings.cs
@@ -0,0 +1,75 @@
+﻿// written by Tariro Grace Shumba
+//2/1/2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Dominion_Of_Kings
+{
+    public class DominionOfKings
+    {
+        // class variables 
+        private string name = "n/a";
+        private int estate = 0;
+        private int province = 0;
+        private int duchy = 0;
+
+
+        //getters and setters 
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Estate
+        {
+            get { return this.estate; }
+            set { this.estate = value; }
+        }
+
+        public int Duchy
+        {
+            get { return this.duchy; }
+            set { this.duchy = value; }
+        }
+
+
+        public int Province
+        {
+            get { return this.province; }
+            set { this.province = value; }
+        }
+
+        //methods 
+
+        public int CalcTotal()
+        {
+            return (Estate * 1) + (Duchy * 3) + (Province * 6);
+        }
+
+        // constructors
+
+        public DominionOfKings(string aName, int aEstate, int aDuchy, int aProvince)
+        {
+            this.name = aName;
+            this.estate = aEstate;
+            this.duchy = aDuchy;
+            this.province = aProvince;
+        }
+
+        // overriding to string
+
+        public override string ToString()
+        {
+            string msg = " ";
+            msg += " King : " + this.Name + " Total points : " + CalcTotal();
+            return msg;
+        }
+
+    }
+      
+
+}
---
+++ The Dominion Of Kings/Program.cs
@@ -0,0 +1,46 @@
+﻿// written by Tariro Grace Shumba
+//2/1/2025
+using System;
+namespace The_Dominion_Of_Kings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompting the kings to enter their names and how homuch stuff they have
+
+            Console.Write("Enter The Kings Name : ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter The Kings Estates : ");
+            int estates = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter The Kings Duchies : ");
+            int duchy = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter The Kings Province : ");
+            int province = Convert.ToInt32(Console.ReadLine());
+
+            // creating a list to put all the information in
+
+            List<DominionOfKings> aListOfKings = new List<DominionOfKings>();
+
+            // creating instances of your class 
+
+            DominionOfKings aKing1 = new DominionOfKings(name, estates, duchy, province);
+
+
+
+            // populating the list
+            aListOfKings.Add(aKing1);
+
+            // printing each item in the list according to our ToString)
+
+            foreach(DominionOfKings aKing in aListOfKings)
+            {
+                Console.WriteLine(aKing.ToString());
+            }
+
+        }
+    }
+}
