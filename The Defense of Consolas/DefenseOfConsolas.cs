a4da428f34e891dcaf08c4b63fe92ac5b06b1668
Tariro Grace Shumba <tarirograceshumba2@gmail.com>
2025-02-02 14:27:35 -0600

    Creating the Defense of Consolas

---
+++ The Defense of Consolas/DefenseOfConsolas.cs
@@ -0,0 +1,63 @@
+﻿//written by Tariro Grace Shumba
+// 2/1/2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Defense_of_Consolas
+{
+    public class DefenseOfConsolas
+    {
+        // class variables 
+        private int row = 0;
+        private int column = 0;
+
+        // getters and setters
+        public int Row
+        {
+            get { return this.row; }
+            set { this.row = value; }
+        }
+
+        public int Column
+        {
+            get { return this.column; }
+            set { this.column = value; }
+        }
+
+        // methods
+
+        public int DeploymentRow()
+        {
+            int deploymentRow = this.Row;
+            return deploymentRow;
+        }
+        public int DeploymentCol()
+        {
+            int deploymentCol = this.Column;
+            return deploymentCol;
+        }
+
+
+        // constructors 
+
+        public DefenseOfConsolas(int aRow, int aColumn)
+        {
+            this.Row = aRow;
+            this.Column = aColumn;
+
+        }
+        //overriding tostring 
+
+        public override string ToString()
+        {
+            string msg = " ";
+            msg += $"Deploy to :\n ({DeploymentRow()},{DeploymentCol() -1} ) \n ({DeploymentRow() - 1},{DeploymentCol()}) " +
+                $"\n ({DeploymentRow() - 1},{DeploymentCol() + 1}) \n ({DeploymentRow() + 1},{DeploymentCol() }) \n";
+            return msg;
+        }
+
+    }
+}
---
+++ The Defense of Consolas/Program.cs
@@ -0,0 +1,39 @@
+﻿//written by Tariro Grace Shumba
+// 2/1/2025
+
+namespace The_Defense_of_Consolas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // changing the window title 
+            Console.Title = "Defense of Consolas";
+
+            // prompting the user to enter the row and column
+            Console.Write("Target Row? ");
+            int row = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Target Column? ");
+            int column = Convert.ToInt32(Console.ReadLine());
+
+            // creating an instance of the class 
+            DefenseOfConsolas aDefense = new DefenseOfConsolas(row,column);
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // printing the deployment 
+
+            Console.WriteLine(aDefense.ToString());
+
+            // sounds for when the results have been computed and displayed
+            Console.Beep(540 , 1500);
+
+
+
+
+
+        }
+    }
+}
